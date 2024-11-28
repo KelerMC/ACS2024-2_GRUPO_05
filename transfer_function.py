c83001b99fcf6e51ad0c10ba29360290d7612ee1
AdrianIsmaelQuispeFajardo <adrianismael.quispefajardo@gmail.com>
2024-11-27 21:00:29 -0500

    equation render

--- transfer_function.py
+++ transfer_function.py
@@ -1,2 +1,12 @@
 import sympy as sp
 import matplotlib.pyplot as plt
+
+def display_equation(expr, title=""):
+    """Función para mostrar ecuaciones en LaTeX"""
+    plt.figure(figsize=(10, 2))
+    plt.text(0.5, 0.5, f"${sp.latex(expr)}$",
+             fontsize=14, ha='center', va='center')
+    plt.axis('off')
+    if title:
+        plt.title(title)
+    plt.show()
