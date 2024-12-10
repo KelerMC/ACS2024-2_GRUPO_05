34e6c3a3120933b9f41222dbfde9d06abc2501c1
AdrianIsmaelQuispeFajardo <adrianismael.quispefajardo@gmail.com>
2024-12-10 02:58:57 -0500

    Add system dynamics equations with PID control logic

--- cart_pole_genetic_controller.py
+++ cart_pole_genetic_controller.py
@@ -31,3 +31,33 @@ class CartPoleSystem:
 
         self.x_ref = 0.0
         self.theta_ref = 0.0
+
+    def system_dynamics(self, state, t):
+        x, theta, x_dot, theta_dot = state
+
+        theta_error = theta - self.theta_ref
+        x_error = x - self.x_ref
+
+        dt = 0.01
+        self.pendulum_pid['integral_error'] += theta_error * dt
+        self.cart_pid['integral_error'] += x_error * dt
+
+        pendulum_control = (self.pendulum_pid['kp'] * theta_error +
+                            self.pendulum_pid['kd'] * theta_dot)
+
+        cart_control = (self.cart_pid['kp'] * x_error +
+                        self.cart_pid['kd'] * x_dot)
+
+        F = cart_control + pendulum_control
+
+        M = np.array([[self.M + self.m, self.m * self.l * np.cos(theta)],
+                      [self.m * self.l * np.cos(theta), self.m * self.l ** 2]])
+
+        C = np.array([[-self.m * self.l * theta_dot ** 2 * np.sin(theta) + F],
+                      [self.m * self.g * self.l * np.sin(theta)]])
+
+        acc = np.linalg.solve(M, C)
+        x_ddot = acc[0][0]
+        theta_ddot = acc[1][0]
+
+        return [x_dot, theta_dot, x_ddot, theta_ddot]
